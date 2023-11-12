ab80b0be7765ae5007b5b51e7dc8eaf626bbfb5e
hanniahaydar <148922406+hanniahaydar@users.noreply.github.com>
2023-11-12 02:13:00 -0600

    nueva clase controladora y lineas de código

---
+++ PracticasPoo/src/Practica7/Controladora.java
@@ -0,0 +1,11 @@
+package Practica7;
+
+import java.util.Scanner
+import java.io.*;
+public class Controladora {
+	
+	public class void main (String [] args) {
+		WebMaker generador = new WebMaker();
+	}
+
+}
