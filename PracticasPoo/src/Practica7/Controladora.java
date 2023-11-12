a81761ee98085e363277e88a85e35353bca60030
hanniahaydar <148922406+hanniahaydar@users.noreply.github.com>
2023-11-12 04:20:59 -0600

    nuevo main

--- PracticasPoo/src/Practica7/Controladora.java
+++ PracticasPoo/src/Practica7/Controladora.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.io.*;
 public class Controladora{
 	
-	public static void main (String [] args){
+	public static void main (String [] args) throws IOException{
 		// WebMaker generador = new WebMaker();
 		// PrintWriter	escritor = new PrintWriter("index.txt");
 		// Scanner	lector = new Scanner(new FileReader("form.txt"));
@@ -22,6 +22,22 @@ public class Controladora{
            //escritor.println(generador.imprimirCierre());
            //lector.close();
            //escritor.close();
-          
+         
+		
+		
+		DataOutputStream escritor	=	new	DataOutputStream
+				(new FileOutputStream("hola.bin"));
+	escritor.writeInt(33465);
+	escritor.writeChar('a');
+	escritor.writeChar('z');
+	escritor.writeShort(10);
+	escritor.close();
+	DataInputStream	lector =	new	DataInputStream
+				(new FileInputStream("hola.bin"));
+	System.out.println(lector.readInt());
+	System.out.println(lector.readChar());
+	System.out.println(lector.readChar());
+	System.out.println(lector.readShort());
+	escritor.close();
 	}
 }
